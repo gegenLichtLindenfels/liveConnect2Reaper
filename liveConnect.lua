c6d2e09a3a035ce095ae2d844b70c52e80f6e4e4
lukas-runge <lukas-runge@arcor.de>
2020-12-20 00:55:13 +0100

    👕 linted with LuaHelper for VSCode

--- liveConnect.lua
+++ liveConnect.lua
@@ -1,18 +1,18 @@
 local print = Printf;
 local function Error(m)
-    local options = {
-        title="Error",
-        message=m,
-        commands={
-            {value=0, name="OKAY"}
-        }
-    }
-    local r = MessageBox(options)
-end;
+  local options = {
+    title = "Error",
+    message = m,
+    commands = {{
+      value = 0,
+      name = "OKAY"
+    }}
+  }
+  local r = MessageBox(options)
+end
 
 -- Load HTTP package
 local http = require("http")
-
 
 -- Cache responses
 local lastResponse;
@@ -20,36 +20,50 @@ local last = {};
 
 function Main()
 
-    local seqNo = TextInput("Enter sequence number", 1);
-    if(not seqNo or not tonumber(seqNo)) then Error("Invalid sequence number"); return; end;
-    local seq = Root().ShowData.DataPools.Default.Sequences[tonumber(seqNo)];
-    if(not seq.no) then Error("Sequence does not exist"); return; end;
-
-    local codeNo = TextInput("Enter timecode number", 1);
-    if(not codeNo or not tonumber(codeNo)) then Error("Invalid timecode number"); return; end;
-    local code = Root().ShowData.DataPools.Default.Sequences[tonumber(codeNo)];
-    if(not code.no) then Error("Timecode does not exist"); return; end;
-    
-    while true do
-  
-      local response = http.request('http://localhost:18080/_/MARKER');
-
-      for name, i, time, color in response:gmatch('MARKER\t([^\t]*)\t([^\t]+)\t([^\t]+)\t([^\t]+)\n') do
-        local cueRef = "Sequence " .. seqNo .. " Cue " .. i;
-        if(last[i] ~= time) then
-          print("Marker %s at %s: %s", i, time, name)
-          if (seq.name == "Sequence "..seq.no) then
-            Cmd('Set Timecode ' .. code.no .. '.*."<Sequence ' .. seq.no .. '>".*.*.' .. i .. ' Property Time ' .. time)
-          else
-            Cmd('Set Timecode ' .. code.no .. '.*."' .. seq.name .. '".*.*.' .. i .. ' Property Time ' .. time)
-          end
-        end; last[i] = time;  
-      end; lastResponse = response;
-  
-    coroutine.yield(3)
-    end
-  
+  local seqNo = TextInput("Enter sequence number", 1);
+  if (not seqNo or not tonumber(seqNo)) then
+    Error("Invalid sequence number");
+    return;
   end
+  local seq = Root().ShowData.DataPools.Default.Sequences[tonumber(seqNo)];
+  if (not seq.no) then
+    Error("Sequence does not exist");
+    return;
+  end
+
+  local codeNo = TextInput("Enter timecode number", 1);
+  if (not codeNo or not tonumber(codeNo)) then
+    Error("Invalid timecode number");
+    return;
+  end
+  local code = Root().ShowData.DataPools.Default.Sequences[tonumber(codeNo)];
+  if (not code.no) then
+    Error("Timecode does not exist");
+    return;
+  end
+
+  while true do
+
+    local response = http.request('http://localhost:18080/_/MARKER');
+
+    for name, i, time, color in response:gmatch('MARKER\t([^\t]*)\t([^\t]+)\t([^\t]+)\t([^\t]+)\n') do
+      local cueRef = "Sequence " .. seqNo .. " Cue " .. i;
+      if (last[i] ~= time) then
+        print("Marker %s at %s: %s", i, time, name)
+        if (seq.name == "Sequence " .. seq.no) then
+          Cmd('Set Timecode ' .. code.no .. '.*."<Sequence ' .. seq.no .. '>".*.*.' .. i .. ' Property Time ' .. time)
+        else
+          Cmd('Set Timecode ' .. code.no .. '.*."' .. seq.name .. '".*.*.' .. i .. ' Property Time ' .. time)
+        end
+      end
+      last[i] = time;
+    end
+    lastResponse = response;
+
+    coroutine.yield(3)
+  end
+
+end
 
 -- Main Loop
 return Main
