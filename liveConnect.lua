326b9aa5262b4e47d75c9f6b5e67f1416d1cb637
lukas-runge <lukas-runge@arcor.de>
2020-12-20 00:56:26 +0100

    💡 added helpful comments for documentation

--- liveConnect.lua
+++ liveConnect.lua
@@ -1,3 +1,4 @@
+-- define helper functions
 local print = Printf
 local function Error(m)
   local options = {
@@ -11,40 +12,49 @@ local function Error(m)
   local r = MessageBox(options)
 end
 
--- Load HTTP package
+-- load HTTP package
 local http = require("http")
 
+-- cache responses
 local lastResponse
 local last = {}
 
+-- define main function
 function Main()
 
+  -- get sequence number by user input 
   local seqNo = TextInput("Enter sequence number", 1)
   if (not seqNo or not tonumber(seqNo)) then
     Error("Invalid sequence number")
     return
   end
+  -- validate user input for sequence number
   local seq = Root().ShowData.DataPools.Default.Sequences[tonumber(seqNo)]
   if (not seq.no) then
     Error("Sequence does not exist")
     return
   end
 
+  -- get timecode number by user input
   local codeNo = TextInput("Enter timecode number", 1)
   if (not codeNo or not tonumber(codeNo)) then
     Error("Invalid timecode number")
     return
   end
+  -- validate user input for timecode number
   local code = Root().ShowData.DataPools.Default.Sequences[tonumber(codeNo)]
   if (not code.no) then
     Error("Timecode does not exist")
     return
   end
 
+  -- define loop for continuous sychronisation with reaper
   while true do
 
+    -- get marker response from reaper
     local response = http.request('http://localhost:18080/_/MARKER')
 
+    -- pasrse response and tell MA3 about changes
     for name, i, time, color in response:gmatch('MARKER\t([^\t]*)\t([^\t]+)\t([^\t]+)\t([^\t]+)\n') do
       local cueRef = "Sequence " .. seqNo .. " Cue " .. i
       if (last[i] ~= time) then
@@ -59,10 +69,11 @@ function Main()
     end
     lastResponse = response
 
+    -- let other processes continue their job for 3 seconds till checking back with reaper agian
     coroutine.yield(3)
   end
 
 end
 
--- Main Loop
+-- run main funtion
 return Main
